e1d180141f000e9f6f995619e357b8ceae93ecbf
JJ Fullmer <ldscellist@Gmail.com>
2014-06-14 17:24:55 -0600

    yay

--- checkout_view.php
+++ checkout_view.php
@@ -1,20 +1,9 @@
 <?php
-include 'db_connect.php';
 $query="SELECT type,byusticker,firstname,lastname,status,dueDate,intendedLocation,date FROM Checkout_item JOIN User on User.userId=Checkout_item.currentUser JOIN Purchase_record on Purchase_record.id=Checkout_item.purchaseRecordId ORDER BY type";
 $result=  mysql_query($query);
+include 'header.php'; 
 ?>
 <html>
-    <head>
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-	<link href="css/style.css" rel="stylesheet">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<script type="text/javascript" src="js/jquery.min.js"></script>
-	<script type="text/javascript" src="js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="js/scripts.js"></script>
-	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-	<link href="css/bootstrap-responsive.css" rel="stylesheet">
-    </head>
-    <body>
         <div class="container">
             <div class="row">
                 <div class="col-xs-12">
@@ -58,7 +47,7 @@ function status_string($status){
     switch ($status){
         case 0:return "Returned";
         case 1:return "Loaned";
-        case 2: return "Given";
+        case 2: return "Given Away";
         case 3: return "Lost";
         default: return "Undefined";
     }
---
+++ header.php
@@ -0,0 +1,55 @@
+<?php
+include 'db_connect.php';
+echo'
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta http-equiv="content-type" content="text/json; charset=utf-8">
+        <title>Dashboard &gt; Dashboard &gt; FOG &gt; Open Source Computer Cloning Solution</title>
+        <link rel="stylesheet" type="text/css" href="css/calendar/calendar-win2k-1.css">
+        <link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+        <link rel="stylesheet" type="text/css" href="css/jquery.organicTabs.css">
+        <!--<link rel="stylesheet" type="text/css" href="css/FOG_THEME" />-->
+        <link rel="stylesheet" type="text/css" href="css/fog.css">
+        <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/scripts.js"></script>
+        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="css/bootstrap-responsive.css" rel="stylesheet">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/scripts.js"></script>
+	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<link href="css/bootstrap-responsive.css" rel="stylesheet">
+    </head>
+    <body>
+    <div id="header">
+        <div id="logo">
+            <h1><a href="/fog/management/index.php"><img src="images/fog-logo.png" original-title="Home"><sup>1.0.1</sup></a></h1>
+            <h2>Open Source Computer Cloning Solution</h2>
+        </div>
+        <div id="menu">
+            <ul>
+                <li><a href="/fog/management/index.php?node=home" title=""><span class="nav-label">Home</span><img src="images/icon-home.png" alt="Home"></a></li>
+                <li><a href="/fog/management/index.php?node=users" title=""><span class="nav-label">User Management</span><img src="images/icon-users.png" alt="User Management"></a></li>
+                <li><a href="/fog/management/index.php?node=host" title=""><span class="nav-label">Host Management</span><img src="images/icon-host.png" alt="Host Management"></a></li>
+                <li><a href="/fog/management/index.php?node=group" title=""><span class="nav-label">Group Management</span><img src="images/icon-group.png" alt="Group Management"></a></li>
+                <li><a href="/fog/management/index.php?node=images" title=""><span class="nav-label">Image Management</span><img src="images/icon-images.png" alt="Image Management"></a></li>
+                <li><a href="/fog/management/index.php?node=storage" title=""><span class="nav-label">Storage Management</span><img src="images/icon-storage.png" alt="Storage Management"></a></li>
+                <li><a href="/fog/management/index.php?node=snapin" title=""><span class="nav-label">Snapin Management</span><img src="images/icon-snapin.png" alt="Snapin Management"></a></li>
+                <li><a href="/fog/management/index.php?node=printer" title=""><span class="nav-label">Printer Management</span><img src="images/icon-printer.png" alt="Printer Management"></a></li>
+                <li><a href="/fog/management/index.php?node=service" title=""><span class="nav-label">Service Management</span><img src="images/icon-service.png" alt="Service Management"></a></li>
+                <li><a href="/fog/management/index.php?node=tasks" title=""><span class="nav-label">Task Management</span><img src="images/icon-tasks.png" alt="Task Management"></a></li>
+                <li><a href="/fog/management/index.php?node=report" title=""><span class="nav-label">Reports</span><img src="images/icon-report.png" alt="Reports"></a></li>
+                <li><a href="/fog/management/index.php?node=about" title=""><span class="nav-label">FOG Configuration</span><img src="images/icon-about.png" alt="FOG Configuration"></a></li>
+                <li><a href="/fog/management/index.php?node=logout" title=""><span class="nav-label">Logout</span><img src="images/icon-logout.png" alt="Logout"></a></li>
+            </ul>
+        </div>
+    </div>';
+    ?>
